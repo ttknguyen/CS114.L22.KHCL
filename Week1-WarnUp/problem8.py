93947276f8d5e22fd7f48a86a0a2dec5c3c2874a
ttknguyen <19520188@gm.uit.edu.vn>
2021-03-04 23:48:24 +0700

    Solving problems week 1

--- Week1-WarnUp/problem8.py
+++ Week1-WarnUp/problem8.py
@@ -1,36 +1,21 @@
 def posB(n, k, p, q):
-    if (k == n): return [-1]
+    posA = 2 * (p-1) + q
+    pos1 = posA - k
+    pos2 = posA + k
+    if(pos1 > 0):
+        print((pos1+1)//2, q if k % 2 == 0 else q % 2 + 1) 
+        return
 
-    tmp = 2 * (p-1) + q
-    codeA =  tmp % k 
-    if (codeA == 0): codeA = k
+    if(pos2 <= n):
+        print((pos2+1) // 2, q if k % 2 == 0 else q % 2 + 1)
+        return
 
-    pos1 = tmp - k
-    if (pos1 > 0): 
-        if (pos1 % 2 == 0):
-            pos1 = [ ( (pos1-2) // 2) + 1, 2]
-        else:
-            pos1 = [ ( (pos1-1) // 2) + 1, 1]
-    else: pos1 = [1e9, 2]
-
-    pos2 = tmp + k
-    if (pos2 <=  n): 
-        if (pos2 % 2 == 0):
-            pos2 = [ ( (pos2-2) // 2) + 1, 2]
-        else:
-            pos2 = [ ( (pos2-1) // 2) + 1, 1]
-    else: pos2 = [1e9, 2]
-
-    if (pos2[0] - p < pos1[0] ):
-        return pos2
-    if (p - pos1[0] <= pos2[0] ):
-        return pos1
-    return [-1]
-
+    print(-1)
+    return
 
 # Drive code
 n = int(input())
 k = int(input())
 p = int(input())
 q = int(input())
-print(*posB(n, k, p, q), end = ' ')
+posB(n, k, p, q)
